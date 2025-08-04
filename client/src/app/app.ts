5d846a83c5ceb61f126efdba1f55dc5752c37ec4
Likhon-baRoy <likhonhere007@proton.me>
2025-08-04 23:41:04 +0600

    use Angular HTTP client service, add ngx-bootstrap

---
+++ client/src/app/app.ts
@@ -0,0 +1,24 @@
+import { Component, signal, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  http = inject(HttpClient);
+  // Using signal to create a reactive title property
+  protected readonly title = signal('client');
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request has completed")
+    })
+  }
+}
