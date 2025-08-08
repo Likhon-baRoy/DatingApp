30d09b980bc353c6e64e116ff5a0f6ac90091694
Likhon-baRoy <likhonhere007@proton.me>
2025-08-09 02:36:26 +0600

    make the form functional using Angular

--- client/src/app/nav/nav.ts
+++ client/src/app/nav/nav.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  model: any = {};
 
+  login() {
+    console.log(this.model);
+  }
 }
