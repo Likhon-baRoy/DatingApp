846ebc5e520cf40386c24fda40e549c3e0f4d283
Likhon-baRoy <likhonhere007@proton.me>
2025-08-13 01:15:04 +0600

    added Register form

---
+++ client/src/app/register/register.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+  model: any = {}
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}
