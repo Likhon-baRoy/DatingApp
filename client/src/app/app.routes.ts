ebeba2aa654d3f15412d78ee5cd04121786062f8
Likhon-baRoy <likhonhere007@proton.me>
2025-08-15 06:34:20 +0600

    create dummy route to protect multiple route esily

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,10 +7,17 @@ import { Messages } from './messages/messages';
 import { authGuard } from './_guards/auth-guard';
 
 export const routes: Routes = [
-    {path: '', component: Home},
-    {path: 'members', component: MemberList, canActivate: [authGuard]},
-    {path: 'members/:id', component: MemberDetail},
-    {path: 'lists', component: Lists},
-    {path: 'messages', component: Messages},
-    {path: '**', component: Home, pathMatch: 'full'}
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberList},
+            { path: 'members/:id', component: MemberDetail },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    },
+    { path: '**', component: Home, pathMatch: 'full' }
 ];
