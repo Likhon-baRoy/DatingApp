44af38f0bf3a6df5b396d08e41fb7a130e0aead9
Likhon-baRoy <likhonhere007@proton.me>
2025-08-15 06:15:54 +0600

    implement user authenticate feature from Angular frontend side

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { MemberList } from './members/member-list/member-list';
 import { MemberDetail } from './members/member-detail/member-detail';
 import { Lists } from './lists/lists';
 import { Messages } from './messages/messages';
+import { authGuard } from './_guards/auth-guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'members', component: MemberList},
+    {path: 'members', component: MemberList, canActivate: [authGuard]},
     {path: 'members/:id', component: MemberDetail},
     {path: 'lists', component: Lists},
     {path: 'messages', component: Messages},
