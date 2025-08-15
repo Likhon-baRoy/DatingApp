ba8ce7bd2c46ba715e83e32a076f7bdee5b2ef88
Likhon-baRoy <likhonhere007@proton.me>
2025-08-15 06:48:25 +0600

    add new theme using `npm bootswatch`

--- client/src/app/nav/nav.ts
+++ client/src/app/nav/nav.ts
@@ -4,10 +4,11 @@ import { Account } from '../_services/account';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
