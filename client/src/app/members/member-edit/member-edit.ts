8ba4d9531474b7a2641e3d542e6374ca27f6e080
Likhon-baRoy <likhonhere007@proton.me>
2025-08-19 15:46:43 +0600

    add update functionality & can deactivate route guard

--- client/src/app/members/member-edit/member-edit.ts
+++ client/src/app/members/member-edit/member-edit.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { Account } from '../../_services/account';
 import { Members } from '../../_services/members';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
@@ -12,9 +13,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './member-edit.css'
 })
 export class MemberEdit implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event:any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
   member?: Member;
   private accountService = inject(Account);
   private memberService = inject(Members);
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
     this.loadMember();
@@ -26,5 +35,11 @@ export class MemberEdit implements OnInit {
     this.memberService.getMember(user.username).subscribe({
       next: member => this.member = member
     })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
