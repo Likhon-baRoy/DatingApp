faea0e5e5f14ef9a7d54c6a2a0067def6489ee21
Likhon-baRoy <likhonhere007@proton.me>
2025-08-21 21:24:17 +0600

    update register API to take additional fields

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,10 +6,15 @@ public class RegisterDto
 {
     [Required]
     [MaxLength(20)]
-    
     public string Username { get; set; } = string.Empty;
 
+    [Required] public string KnownAs { get; set; } = string.Empty;
+    [Required] public string Gender { get; set; } = string.Empty;
+    [Required] public string DateOfBirth { get; set; } = string.Empty;
+    [Required] public string City { get; set; } = string.Empty;
+    [Required] public string Country { get; set; } = string.Empty;
+
     [Required]
-    [StringLength(20, MinimumLength = 6)]
+    [StringLength(15, MinimumLength = 4)]
     public string Password { get; set; } = string.Empty;
 }
