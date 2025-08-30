4066b3262be300cc9a948406a0dbc93249e89b42
Likhon-baRoy <likhonhere007@proton.me>
2025-08-31 01:25:00 +0600

    add likes component

--- client/src/app/lists/lists.ts
+++ client/src/app/lists/lists.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Likes } from '../_services/likes';
+import { Member } from '../_models/member';
+import { ButtonsModule } from "ngx-bootstrap/buttons";
+import { FormsModule } from '@angular/forms';
+import { MemberCard } from "../members/member-card/member-card";
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [ButtonsModule, FormsModule, MemberCard],
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
-export class Lists {
+export class Lists implements OnInit {
+  private likesService = inject(Likes);
+  members: Member[] = [];
+  predicate = 'liked';
 
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  getTitle() {
+    switch (this.predicate) {
+      case 'liked': return 'Members you like';
+      case 'likedBy': return 'Members who like you'
+      default: return 'Mutual'
+    }
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members = members
+    })
+  }
 }
