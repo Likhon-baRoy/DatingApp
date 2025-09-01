198585bf605ed30fb24f151e6bc8f308b1dd9348
Likhon-baRoy <likhonhere007@proton.me>
2025-09-02 02:55:00 +0600

    add pagination likes on the client side

--- client/src/app/lists/lists.ts
+++ client/src/app/lists/lists.ts
@@ -1,20 +1,21 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Likes } from '../_services/likes';
-import { Member } from '../_models/member';
 import { ButtonsModule } from "ngx-bootstrap/buttons";
 import { FormsModule } from '@angular/forms';
 import { MemberCard } from "../members/member-card/member-card";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
-  imports: [ButtonsModule, FormsModule, MemberCard],
+  imports: [ButtonsModule, FormsModule, MemberCard, PaginationModule],
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
-export class Lists implements OnInit {
-  private likesService = inject(Likes);
-  members: Member[] = [];
+export class Lists implements OnInit, OnDestroy {
+  likesService = inject(Likes);
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
     this.loadLikes();
@@ -29,8 +30,17 @@ export class Lists implements OnInit {
   }
 
   loadLikes() {
-    this.likesService.getLikes(this.predicate).subscribe({
-      next: members => this.members = members
-    })
+    this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);
   }
 }
