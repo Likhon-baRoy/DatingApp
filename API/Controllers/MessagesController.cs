cf7b51ae7ed60ec5abd1344b26cb756f8783ab65
Likhon-baRoy <likhonhere007@proton.me>
2025-09-06 12:06:44 +0600

    getting the messages from the Repo

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ namespace API.Controllers;
 
 public class MessagesController(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
+  [HttpPost]
   public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
   {
     var username = User.GetUsername();
@@ -36,5 +38,17 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
     if (await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
 
     return BadRequest("Failed to save message");
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams)
+  {
+    messageParams.Username = User.GetUsername();
+
+    var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+    Response.AddPaginationHeader(messages);
+
+    return messages;
   }
 }
