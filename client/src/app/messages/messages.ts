030fef9af4b6327dceb18887ac45460a53c47e51
Likhon-baRoy <likhonhere007@proton.me>
2025-09-06 23:18:45 +0600

    setting up the angular app for messaging

--- client/src/app/messages/messages.ts
+++ client/src/app/messages/messages.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessageService } from '../_services/message';
 
 @Component({
   selector: 'app-messages',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
-export class Messages {
+export class Messages implements OnInit {
+  messageService = inject(MessageService);
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 5;
 
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
