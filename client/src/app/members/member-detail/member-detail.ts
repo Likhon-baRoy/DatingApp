fd4a9a7b774b57a3e7848b04859c7b0d8d7b1735
Likhon-baRoy <likhonhere007@proton.me>
2025-09-12 02:51:36 +0600

    fixed  redirecting from message-icon to chat tab

--- client/src/app/members/member-detail/member-detail.ts
+++ client/src/app/members/member-detail/member-detail.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, inject, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
@@ -6,7 +6,6 @@ import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
 import { MemberMessages } from "../member-messages/member-messages";
-import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message';
 import { Presence } from '../../_services/presence';
 import { Account } from '../../_services/account';
@@ -17,8 +16,8 @@ import { Account } from '../../_services/account';
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
-export class MemberDetail implements OnInit, OnDestroy, AfterViewInit {
-  @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
+export class MemberDetail implements OnInit, OnDestroy {
+  @ViewChild('memberTabs', { static: false }) memberTabs?: TabsetComponent;
   private messageService = inject(MessageService);
   private accountService = inject(Account);
   presenceService = inject(Presence);
@@ -27,7 +26,7 @@ export class MemberDetail implements OnInit, OnDestroy, AfterViewInit {
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.route.data.subscribe({
       next: data => {
         this.member = data['member'];
@@ -35,13 +34,15 @@ export class MemberDetail implements OnInit, OnDestroy, AfterViewInit {
           this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
         })
       }
-    })
+    });
   }
 
   ngAfterViewInit(): void {
     this.route.queryParams.subscribe({
       next: params => {
-        params['tab'] && this.selectTab(params['tab']);
+        if (params['tab']) {
+          this.selectTab(params['tab']);
+        }
       }
     });
   }
@@ -49,19 +50,28 @@ export class MemberDetail implements OnInit, OnDestroy, AfterViewInit {
   selectTab(heading: string) {
     if (this.memberTabs) {
       const messageTab = this.memberTabs.tabs.find(x => x.heading === heading);
-      if (messageTab) messageTab.active = true;
+      if (messageTab) {
+        setTimeout(() => {
+          messageTab.active = true;
+        });
+      }
     }
   }
 
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
+
     if (this.activeTab.heading === 'Messages' && this.member) {
-      const user = this.accountService.currentUser();
-      if (!user) return;
-      this.messageService.createHubConnection(user, this.member.username);
+      this.loadMessages();
     } else {
       this.messageService.stopHubConnection();
     }
+  }
+
+  loadMessages() {
+    const user = this.accountService.currentUser();
+    if (!user || !this.member) return;
+    this.messageService.createHubConnection(user, this.member.username);
   }
 
   ngOnDestroy(): void {
